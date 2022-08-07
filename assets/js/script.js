413bfb0b0bc6d2b3a9b76be486e4f0019b3e87f5
Light Helix <107587111+LittMarque27@users.noreply.github.com>
2022-08-07 19:52:29 -0400

    Added inputs, buttons, and local storage for tasks.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,19 @@ var dateTime = moment().format("MMM Do, YYYY");
 //var currentHOU 
 var currentHour = moment().format("H");
 console.log(currentHour)
-//var past
-//var future
 
 //array hours slots
-var hours = $(".container").children().children(".hour");
+// var hours = $(".container").children().children(".hour");
 
-var timeBlock = $(".hour").html().trim()
-console.log(hours.html().trim())
+// var timeBlock = $(".hour").html().trim()
+// console.log(hours.html().trim())
 //array text slots
-var tasks = $("#customTime")
+// var tasks = $("#customTime")
 
-console.log(hours)
+// console.log(hours)
 
-var thisTimeEl = $(".thisTime")
-console.log(thisTimeEl.html())
+// var thisTimeEl = $(".thisTime")
+// console.log(thisTimeEl.html())
 
 var timeSlots = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 console.log(timeSlots);
@@ -31,7 +29,7 @@ function timeColor() {
     for (var i = 0; i < timeSlots.length; i++) {
 
         var taskBlock = $(".container").children().eq(i).children().eq(1)
-        
+
         if ((timeSlots[i]) < currentHour) {
             taskBlock.addClass("past")
         } else if ((timeSlots[i]) > currentHour) {
@@ -44,10 +42,32 @@ function timeColor() {
 
 timeColor()
 
+var containerEl = $(".container");
+
+containerEl.on("click", ".saveBtn", function (event) {
+    var inputClass = event.currentTarget.attributes[1].nodeValue;
+    console.log(inputClass)
+    var inputValue = $("."+ inputClass).val()
+    console.log(inputValue)
+    localStorage.setItem(inputClass, inputValue)
+})
+
+function displayTask() {
+    $(".9am").val(localStorage.getItem("9am"))
+    $(".10am").val(localStorage.getItem("10am"))
+    $(".11am").val(localStorage.getItem("11am"))
+    $(".12pm").val(localStorage.getItem("12pm"))
+    $(".1pm").val(localStorage.getItem("1pm"))
+    $(".2pm").val(localStorage.getItem("2pm"))
+    $(".3pm").val(localStorage.getItem("3pm"))
+    $(".4pm").val(localStorage.getItem("4pm"))
+    $(".5pm").val(localStorage.getItem("5pm"))
+}
+
+displayTask()
 
 
-
-
+//localStorage.clear
 // if($(".container").children().eq(0).children().eq(0) !== currentHOUR) {
 //     $(".container").children().eq(0).children().eq(1).addClass("future")
 // }
