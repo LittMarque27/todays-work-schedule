ab74ca7c3797b4e22a4124a82ef4e44a21df12a3
Light Helix <107587111+LittMarque27@users.noreply.github.com>
2022-08-07 21:18:17 -0400

    Cleaned up styling, drafted READNE, and entered comments

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# todays-work-schedule
+# Today’s Work Schedule
+
+We all know that your average workday can be jam-packed at times. It’s hard to make time for daily tasks, long-term projects, meetings, networking, and let’s not forget lunch! That’s why it’s vital that you have the ability to plan out your day with this work scheduler web app.
+
+Take a look at this screenshot for an example of what an organized workday looks like:
+![Full Screenshot of a Workday](./assets/images/schedule-full.png)
+
+## Entering a task
+
+Tasks can be entered for any corresponding time-block by clicking on the input field next to the “Task” label. Type in the description of your task and make sure that you save your task with the blue button on the right. This will save your task even if you leave the page. You can also edit your tasks just as easily by following the same steps.
+
+## Color-coding
+
+Each hour is color coded in relation to the current hour. If the block is gray, then that hour has passed. If it’s green, it’s still an upcoming event. If it’s red, that task or event is happening right now! Get a move on!
+
+## Clear Tasks
+
+Once you’re done with your day you can clear your tasks with the “Clear Tasks” button above the scheduling grid. This will clear your tasks and reload the page.
+
+### Get Organized Now!
+
+[Access your own personal scheduler here](https://littmarque27.github.io/todays-work-schedule/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,18 @@
-//Moment JS
-//var dateTime = now
-var dateTime = moment().format("MMM Do, YYYY");
-//var currentHOU 
+//Moment JS variables
+var dateTime = moment().format("MMM Do, YYYY"); 
 var currentHour = moment().format("H");
 console.log(currentHour)
 
 //array hours slots
-// var hours = $(".container").children().children(".hour");
-
-// var timeBlock = $(".hour").html().trim()
-// console.log(hours.html().trim())
-//array text slots
-// var tasks = $("#customTime")
-
-// console.log(hours)
-
-// var thisTimeEl = $(".thisTime")
-// console.log(thisTimeEl.html())
-
 var timeSlots = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 console.log(timeSlots);
 
 $("#currentDay").text(dateTime);
 
 function timeColor() {
-    
-    
     for (var i = 0; i < timeSlots.length; i++) {
 
-        var taskBlock = $(".container").children().eq(i).children().eq(1)
+        var taskBlock = $(".container").children().eq(i).children().eq(1);
 
         if ((timeSlots[i]) < currentHour) {
             taskBlock.addClass("past")
@@ -37,57 +21,43 @@ function timeColor() {
         } else {
             taskBlock.addClass("present")
         }
-    }
+    };
 };
 
-timeColor()
+timeColor();
+
+//event and functions for task local storage
 
 var containerEl = $(".container");
 
 containerEl.on("click", ".saveBtn", function (event) {
     var inputClass = event.currentTarget.attributes[1].nodeValue;
-    console.log(inputClass)
-    var inputValue = $("."+ inputClass).val()
-    console.log(inputValue)
-    localStorage.setItem(inputClass, inputValue)
-})
+    console.log(inputClass);
+    var inputValue = $("."+ inputClass).val();
+    console.log(inputValue);
+    localStorage.setItem(inputClass, inputValue);
+});
 
 function displayTask() {
-    $(".9am").val(localStorage.getItem("9am"))
-    $(".10am").val(localStorage.getItem("10am"))
-    $(".11am").val(localStorage.getItem("11am"))
-    $(".12pm").val(localStorage.getItem("12pm"))
-    $(".1pm").val(localStorage.getItem("1pm"))
-    $(".2pm").val(localStorage.getItem("2pm"))
-    $(".3pm").val(localStorage.getItem("3pm"))
-    $(".4pm").val(localStorage.getItem("4pm"))
+    $(".9am").val(localStorage.getItem("9am"));
+    $(".10am").val(localStorage.getItem("10am"));
+    $(".11am").val(localStorage.getItem("11am"));
+    $(".12pm").val(localStorage.getItem("12pm"));
+    $(".1pm").val(localStorage.getItem("1pm"));
+    $(".2pm").val(localStorage.getItem("2pm"));
+    $(".3pm").val(localStorage.getItem("3pm"));
+    $(".4pm").val(localStorage.getItem("4pm"));
     $(".5pm").val(localStorage.getItem("5pm"))
-}
+};
 
-displayTask()
+displayTask();
 
+//Button for clearing local storage
 
-//localStorage.clear
-// if($(".container").children().eq(0).children().eq(0) !== currentHOUR) {
-//     $(".container").children().eq(0).children().eq(1).addClass("future")
-// }
+var clearBtn = $("#custom-clear");
 
+clearBtn.on("click", function() {
+    localStorage.clear();
+    location.reload();
+})
 
-// if(9 < currentHour) {
-//     $(".container").children().eq(0).children().eq(1).addClass("past");
-// } else if (9 > currentHour) {
-//     tasks.addClass("future");
-// } else {
-//     tasks.addClass("present");
-// };
-
-    
-
-
-//For loop for checking hour
-
-//call function
-
-//local storage
-
-//Clear local storage???
